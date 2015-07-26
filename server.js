908fd6bb476fe14889a65c626023dcd90b5a5046
Brandon <leebran@onid.oregonstate.edu>
2015-07-26 01:40:21 -0700

    Added server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+// Import dependecies
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+
+// Initilize express app
+var app =  express();
+
+app.get('/scrape', function(req, res){
+    // Scraping happens here
+};
+
+console.log("Server initilizes at port 8080");
+
+// Modularlize our code
+exports = module.exports = app;
